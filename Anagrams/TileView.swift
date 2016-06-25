ed3b8f4d3a81fb037f7a6e850d85445e9b9ad114
jbachelor <jon_bachelor@me.com>
2016-06-24 20:20:58 -0400

    Finished step 6:  App now displays the first anagram letters at the bottom of the screen.

--- Anagrams/TileView.swift
+++ Anagrams/TileView.swift
@@ -25,6 +25,15 @@ class TileView: UIImageView {
         // resize tile
         let scale = sideLength / image.size.width
         self.frame = CGRect(x: 0, y: 0, width: image.size.width * scale, height: image.size.height * scale)
+        
+        // Add a letter on top
+        let letterLabel = UILabel(frame: self.bounds)
+        letterLabel.textAlignment = NSTextAlignment.Center
+        letterLabel.textColor = UIColor.whiteColor()
+        letterLabel.backgroundColor = UIColor.clearColor()
+        letterLabel.text = String(letter).uppercaseString
+        letterLabel.font = UIFont(name: "Verdana-Bold", size: 78.0 * scale)
+        self.addSubview(letterLabel)
     }
     
 }
